bfe17ce18e327d84f4959aa2994313288cbb118c
rallen <rcallen@gmail.com>
2023-01-03 14:02:11 -0800

    split navigation

--- nvim/.config/nvim/lua/user/mappings.lua
+++ nvim/.config/nvim/lua/user/mappings.lua
@@ -7,3 +7,9 @@ vim.g.mapleader = ' ' -- 'vim.g' sets global variables
 
 -- nvim-tree
 map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- split navigation
+map("n", "<C-h>","<C-w><C-h>", opts)
+map("n", "<C-j>","<C-w><C-j>", opts)
+map("n", "<C-k>","<C-w><C-k>", opts)
+map("n", "<C-l>","<C-w><C-l>", opts)
