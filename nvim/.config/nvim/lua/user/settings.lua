6a2f2d7d4e7bfa9a64dfeda1e04d50855a20a2bb
rallen <rcallen@gmail.com>
2023-01-03 16:09:14 -0800

    colorcolumn 80

--- nvim/.config/nvim/lua/user/settings.lua
+++ nvim/.config/nvim/lua/user/settings.lua
@@ -5,7 +5,7 @@ set.smarttab          = true
 set.shiftwidth        = 2
 set.tabstop           = 2
 
-set.hlsearch          = true
+set.hlsearch          = false
 set.incsearch         = true
 set.ignorecase        = true
 set.smartcase         = true
@@ -13,14 +13,16 @@ set.smartcase         = true
 set.splitbelow        = true
 set.splitright        = true
 set.wrap              = false
-set.scrolloff         = 5
+set.scrolloff         = 8
+set.signcolumn        = "yes"
 set.fileencoding      = 'utf-8'
 set.termguicolors     = true
 
 set.relativenumber    = true
 set.cursorline        = true
+set.colorcolumn       = "80"
 
 set.hidden            = true
 
 set.mouse             = 'a'
-set.clipboard        = 'unnamedplus'
+set.clipboard         = 'unnamedplus'
