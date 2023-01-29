9d11805ef7508a9fd3a84f39abda996bf04cfe28
rallen <rcallen@gmail.com>
2023-01-29 11:19:53 -0800

    updates

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-xautolock.sh &
-dunst &
-picom &
-dwmblocks &
+pgrep xautolock.sh  || xautolock.sh &
+pgrep dunst         || dunst &
+pgrep picom         || picom &
+pgrep udiskie       || udiskie &
+pgrep dwmblocks     || dwmblocks &
+pgrep discord       || discord &
+pgrep firefox       || firefox &
