4dd7ee83dee687c7be7dff6ca7b265f5ad5f6dca
Rich <rcallen@gmail.com>
2024-08-03 13:53:00 -0700

    single monitor

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pgrep xautolock.sh  || xautolock.sh &
-pgrep dunst         || dunst &
-pgrep picom         || picom &
-pgrep udiskie       || udiskie &
-pgrep dwmblocks     || dwmblocks &
-pgrep discord       || discord &
-pgrep firefox       || firefox &
+pgrep xautolock.sh || xautolock.sh &
+pgrep dunst || dunst &
+pgrep picom || picom &
+pgrep udiskie || udiskie &
+pgrep dwmblocks || dwmblocks &
+# pgrep discord       || discord &
+# pgrep firefox       || firefox &
 # pgrep -f webcam_light_check.sh || webcam_light_check.sh &
